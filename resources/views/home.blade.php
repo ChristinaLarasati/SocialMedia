ac3f15611b0fd748b3e97cf7c069eca9bd61308b
ChristinaLarasati <larasati.christina@yahoo.com>
2019-07-21 22:28:03 -0600

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <img src="{{ Auth::user()->profile_picture }}" alt="">
+                    WELCOME TO THE CLUB {{ Auth::user()->username}}
+                      <div>
+                        <ul class="nav nav-tabs" role="tablist">
+                          <div class = "btn-toolbar">
+                              <button role="presentation" type="button" class="btn btn-default" class="active" href="#posts" aria-controls="posts" role="tab" data-toggle="tab"> Post </button>
+                              <button role="presentation" type="button" class="btn btn-default" href="#comments" aria-controls="comments" role="tab" data-toggle="tab"> Comments </button>
+                          </div>
+                        <!-- <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+                        <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li> -->
+                        </ul>
+
+                        <div class="tab-content">
+                          <div role="tabpanel" class="tab-pane active" id="posts">
+                            @foreach (Auth::user()->posts as $post)
+                              <div class ="panel panel-default">
+                                <div class="panel-heading">
+                                  <h3 class="panel-title">{{ $post->title }}</h3>
+                                </div>
+                                <div class="panel-body">
+                                  {{ $post->body }}
+                                </div>
+                              </div>
+                            @endforeach
+                          </div>
+
+                          <div role="tabpanel" class="tab-pane active" id="comments">...</div>
+                          <!-- <div role="tabpanel" class="tab-pane" id="messages">...</div>
+                          <div role="tabpanel" class="tab-pane" id="settings">...</div> -->
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="col-sm-6 col-sm-offset-3">
+      @if (Session::has('success'))
+      <div class="alert alert success">
+        <a href='#' class="close" data-dismiss="alert">&times;</a>
+        {{ Session::get('success') }}
+      </div>
+      @endif
+      <form method="post" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <div class="panel panel-default">
+            <div class="panel-body">
+              <div class="form-group {{ $errors -> has('title') ? 'has-error' : '' }}">
+                  <input type="text" name="title" class="form-control" placeholder="Enter your post title">
+                  @if ($errors->has('title'))
+                        <small class="text-danger">{{ $errors->first('title') }}</small>
+                  @endif
+              </div>
+
+              <div class="form-group">
+                <input type="file" class="form-control" name="image">
+              </div>
+
+              <div class ="form-group {{ $errors -> has('body') ? 'has-error' : '' }}">
+                  <textarea name="body" rows="8" cols="80" class="form-control" placeholder="Enter your post"></textarea>
+                  @if ($errors->has('body'))
+                        <small class="text-danger">{{ $errors->first('body') }}</small>
+                  @endif
+              </div>
+              <input type="submit" value="Post" class="btn btn-primary btn-block">
+            </div>
+        </div>
+      </form>
+
+      @foreach ($posts as $post)
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+              Created by : {{ $post->user->username}}, {{ $post -> title }}
+            </h3>
+          </div>
+
+          <div class="panel-body">
+            {{ $post -> body }}
+            @if ($post->image != null)
+                <img src="/images/{{ $post->image }}" alt="Image" width="100%" height="600">
+            @endif
+          </div>
+
+          <div class="panel-footer">
+              <button href="#" type="button" class="btn btn-default">Like</button>
+              <button href="#" type="button" class="btn btn-default">Dislike</button>
+              <button href="#" type="button" class="btn btn-default">Comment</button>
+              <!-- <a href="#" class="btn btn-link">Like</a>
+              <a href="#" class="btn btn-link">Dislike</a>
+              <a href="#" class="btn btn-link">Comment</a> -->
+          </div>
+        </div>
+      @endforeach
+
+    </div<
+  </div>
+@endsection
